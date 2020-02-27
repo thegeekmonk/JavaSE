8c1b85b7b3dcd17a380f270a17481a7cfa41b654
Akhilesh KUmar <akhilesh.h.kumar@accenture.com>
2020-02-27 12:42:56 +0530

    Array of ArrayList

---
+++ Hackerrank Practice/src/code/java/hackerrank/ArrayListEx1.java
@@ -0,0 +1,61 @@
+package code.java.hackerrank;
+
+import java.util.*;
+
+public class ArrayListEx1 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		ArrayList<Integer> list[] = new ArrayList[n];
+		
+	    for(int i = 0;i < n;i++)
+	    {   
+	    	int d = sc.nextInt();
+	    	
+	    	int j = d;
+	    	while(j > 0)
+	    	{ 
+	    	   Integer data = sc.nextInt();	
+	    	   list[i].add(data);
+	    	   
+	    	   j--;
+	    	}	    	
+	    }
+	    
+	    int q = sc.nextInt();
+	    
+	    int x[] = new int[q];
+	    int y[] = new int[q];
+		
+	    //ArrayList<Integer> list[] = new ArrayList<Integer>()[n];
+		
+	    for(int k = 0;k < q;k++)
+	    {
+	    	x[k] = sc.nextInt();
+	    	y[k] = sc.nextInt();
+	    }
+	    
+	    for(int k = 0;k < q;k++)
+	    {
+	    	//if(list[x[k]].get(y[k])) != null)
+	    	
+	    	if(y[k] > (list[x[k]].size()))
+	    		System.out.println("ERROR!");
+	    	
+	    	else
+	    	{
+	    		System.out.println(list[x[k]].get(y[k]));
+	    	}
+	    	
+	    }
+		
+       sc.close();
+	}
+
+}
---
+++ Hackerrank Practice/src/code/java/hackerrank/Test.java
@@ -0,0 +1,25 @@
+package code.java.hackerrank;
+
+import java.util.*;
+
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		list.add(30);
+		list.add(50);
+		list.add(70);
+		
+		System.out.println(list.get(6));
+		
+		
+		System.out.println(list.size());
+				
+
+	}
+
+}
