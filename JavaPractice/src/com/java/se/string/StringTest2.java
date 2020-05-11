1f0d23265b871f87d25487cfee203fca06289bd6
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-05-12 03:00:45 +0530

    String literal testing

--- JavaPractice/src/com/java/se/string/StringTest2.java
+++ JavaPractice/src/com/java/se/string/StringTest2.java
@@ -15,11 +15,15 @@ public class StringTest2
 		String a = "meow";
 		String ab = a+"deal";
 		
+		String cd = a.concat("deal");
+		
 		String abc = "meowdeal";
 		
 		System.out.println("checking equality : "+ab.equals(abc));
 		
 		System.out.println("ab == abc : "+(ab == abc));
+		
+		System.out.println("cd == abc : "+(cd == abc));
 
 	}
 
