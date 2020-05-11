fa1c1d9c3876c8dbefb37c13504a134623d459d9
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-05-12 02:50:12 +0530

    String literal testing

---
+++ JavaPractice/src/com/java/se/string/StringTest2.java
@@ -0,0 +1,26 @@
+package com.java.se.string;
+
+public class StringTest2 
+{
+
+	public StringTest2()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		String a = "meow";
+		String ab = a+"deal";
+		
+		String abc = "meowdeal";
+		
+		System.out.println("checking equality : "+ab.equals(abc));
+		
+		System.out.println("ab == abc : "+(ab == abc));
+
+	}
+
+}
