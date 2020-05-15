a0dd391cc0fb0991f669511a320f4480a2a96d6a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-05-16 04:54:56 +0530

    simply adding file

---
+++ JavaPractice/src/com/java/se/StringTest1.java
@@ -0,0 +1,22 @@
+package com.java.se;
+
+public class StringTest1 
+{
+
+	public StringTest1() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		String s1 = "Sachin";
+		String s2 = new String("Sachin");
+		
+		System.out.println("Both String are Equal : "+s1.equals(s2));
+
+	}
+
+}
---
+++ JavaPractice/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AkhileshPC
+ *
+ */
+module javaPractice {
+}
