41c18228720dc3560182a2eabcbc394e553a7eee
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-06-02 05:36:35 +0530

    HashMap code 1

---
+++ Bench_Java_TechEval/src/com/java/collection/map/HashMap_101.java
@@ -0,0 +1,26 @@
+package com.java.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMap_101 
+{
+
+	public static void main(String[] args) 
+	{
+		Map<Integer,String> map = new HashMap<Integer,String>();
+		
+		map.put(101,"Akhilesh");
+		map.put(201,"Rahul");
+		map.put(301,"Aditya");
+		map.put(103,"Kushal");
+		
+		Set<Map.Entry<Integer,String>> set = map.entrySet();
+		
+		for(Map.Entry<Integer,String> entry : set)
+		   System.out.println(entry.getKey()+" --> "+entry.getValue());
+
+	}
+
+}
