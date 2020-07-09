14b58b1a473b78e3f3cba69bceba2fbb53fc3da0
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-09 13:55:42 +0530

    Preparing for coding interview SAP and BOFA

--- Java_Coding_Interview/src/com/akhilesh/coding/Search_Linear.java
+++ Java_Coding_Interview/src/com/akhilesh/coding/Search_Linear.java
@@ -1,12 +1,27 @@
 package com.akhilesh.coding;
 
 public class Search_Linear 
-{
-
+{   
+//	int arr[] = {14,89,34,56,6,8,82};
+    
+	public static boolean search(int arr[],int num)
+	{
+		for(int i = 0;i < arr.length;i++)
+			if(arr[i] == num)
+				return true;	
+				
+		return false;
+	}
+	
+	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 
+		int arr[] = {34,56,6,9,12,24,41};
+		
+		System.out.println("Search Result for 9 : "+search(arr,9)); 		
+		
 	}
 
 }
