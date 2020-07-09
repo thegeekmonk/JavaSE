f13a2921145129c5138deeb341a314bde42cc797
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-09 21:37:22 +0530

    Serialization : Coding interview SAP and BOFA

--- Java_Coding_Interview/src/com/akhilesh/java/SerializationMain.java
+++ Java_Coding_Interview/src/com/akhilesh/java/SerializationMain.java
@@ -1,12 +1,30 @@
 package com.akhilesh.java;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class SerializationMain 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		// TODO Auto-generated method stub
 
+		StudentSerialization st = new StudentSerialization(101,"Akhilesh Kumar",27);
+		
+		FileOutputStream fout = new FileOutputStream("E:\\student.txt");		
+		ObjectOutputStream out = new ObjectOutputStream(fout);
+		
+		out.writeObject(st);
+		
+		out.flush();
+		out.close();
+		
+		fout.close();
+		
+		System.out.println("Student Class Serialized Successfully !");
 	}
 
 }
