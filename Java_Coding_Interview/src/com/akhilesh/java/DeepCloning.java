4a5613d3a1b0ca802399639e5e5bbbc9805c934c
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-09 23:57:43 +0530

    Cloning : Coding interview SAP and BOFA

--- Java_Coding_Interview/src/com/akhilesh/java/DeepCloning.java
+++ Java_Coding_Interview/src/com/akhilesh/java/DeepCloning.java
@@ -1,12 +1,59 @@
 package com.akhilesh.java;
 
+class Empl
+{
+	 int id;
+	 String department;
+	 
+//	public Employee() 
+//	{
+//		super();
+//		id = 101;
+//		department = "Technology";
+//	}
+//	 
+	 
+}
+
+class Engg implements Cloneable
+{
+	 String name;
+	 
+//	 = "Akhilesh Kumar";
+	
+	Empl e = new Empl();
+	
+	public Object clone() throws CloneNotSupportedException
+	{   
+		Engg eg = (Engg)super.clone();
+		
+		eg.e = new Empl();
+				
+		return eg;		
+	}	
+	
+}
+
 public class DeepCloning 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws CloneNotSupportedException 
 	{
-		// TODO Auto-generated method stub
-         
+		Engineer e1 = new Engineer();
+		
+		e1.name = "Akhilesh";
+		e1.e.id = 101;
+		e1.e.department = "technology";		
+		
+		Engineer e2 = (Engineer)e1.clone();
+		
+//		e2.e.department = "cloud";  //testing Deep cloning feature
+		
+	    System.out.println("Original Object --> ID : "+e1.e.id+" Department : "+e1.e.department+" Name : "+e1.name);
+	    
+	    System.out.println("Cloned Object --> ID : "+e2.e.id+" Department : "+e2.e.department+" Name : "+e2.name);
+
 	}
 
 }
+
