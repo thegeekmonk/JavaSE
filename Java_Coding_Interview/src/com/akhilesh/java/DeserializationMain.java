3be755f67670814fccd9741a2523ee4659789070
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-09 21:45:48 +0530

    DeSerialization : Coding interview SAP and BOFA

--- Java_Coding_Interview/src/com/akhilesh/java/DeserializationMain.java
+++ Java_Coding_Interview/src/com/akhilesh/java/DeserializationMain.java
@@ -2,15 +2,24 @@ package com.akhilesh.java;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class DeserializationMain 
 {
 
-	public static void main(String[] args) throws FileNotFoundException 
+	public static void main(String[] args) throws IOException, ClassNotFoundException 
 	{
 		// TODO Auto-generated method stub
        
-		FileInputStream fin = new FileInputStream("E:\\studnet.txt");
+		
+		FileInputStream fin = new FileInputStream("E:\\student.txt");
+		ObjectInputStream in = new ObjectInputStream(fin);
+		
+		StudentSerialization st = (StudentSerialization)in.readObject();
+		
+		System.out.println("Student ID : "+st.id+" Name : "+st.name+" Age : "+st.age);
+		
 		
 		
 		
