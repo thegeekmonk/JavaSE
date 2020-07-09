1cd6a74f39ba7402cc6717d425e1cf2cad0dcf8e
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-09 23:15:38 +0530

    Cloning : Coding interview SAP and BOFA

---
+++ Java_Coding_Interview/src/com/akhilesh/java/DeepCloning.java
@@ -0,0 +1,10 @@
+package com.akhilesh.java;
+
+public class DeepCloning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java_Coding_Interview/src/com/akhilesh/java/ShallowCloning.java
+++ Java_Coding_Interview/src/com/akhilesh/java/ShallowCloning.java
@@ -2,7 +2,8 @@ package com.akhilesh.java;
 
 public class ShallowCloning {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
