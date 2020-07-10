92a5d5122e71c793c59139896884daf977f41b66
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-07-10 23:44:04 +0530

    SAP sum interview question

--- Java_Coding_Interview/src/com/akhilesh/coding/SAP_Sum.java
+++ Java_Coding_Interview/src/com/akhilesh/coding/SAP_Sum.java
@@ -1,11 +1,39 @@
 package com.akhilesh.coding;
 
-public class SAP_Sum {
+public class SAP_Sum 
+{
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 
+		int arr[] = {1,3,4,8,9};
+        
+		int size = arr.length;		
+		int temp;		
+		int i = 0;
+		
+		int sum = 7;
+		
+		while(size > 0)
+		{
+			temp = arr[i] + arr[size-1];
+			
+			if(temp == sum)
+			{
+				System.out.println("{"+arr[i]+", "+arr[size-1]+" }");
+				break;
+			}
+			else if(temp > sum)
+				--size;
+			else 
+				++i;			
+			
+		}
+		
+		if(i >= size || size <= 0)
+			System.out.println("No possible pair");		
+		
 	}
 
 }
