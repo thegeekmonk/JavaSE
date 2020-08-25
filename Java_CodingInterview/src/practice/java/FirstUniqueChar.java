213edfc10d61093f88eb29ea3fc1e544af7aeceb
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-08-25 14:07:04 +0530

    1st Unique Char

--- Java_CodingInterview/src/practice/java/FirstUniqueChar.java
+++ Java_CodingInterview/src/practice/java/FirstUniqueChar.java
@@ -1,12 +1,20 @@
 package practice.java;
 
+class CountIndex
+{
+	 int count;
+	 int index;
+}
+
+
 public class FirstUniqueChar 
 {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+        
+		
 	}
 
 }
