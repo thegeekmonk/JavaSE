f84082ab81a5797ca3e559e141c2f528d5363357
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-13 19:01:44 +0530

    Recursive Digit sum

---
+++ Java_CodingInterview/src/practice/java/RecursiveDigitSum.java
@@ -0,0 +1,16 @@
+package practice.java;
+
+public class RecursiveDigitSum 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		String str = "43589";
+		
+		
+		
+	}
+
+}
