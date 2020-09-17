5b98b44600e19abe5368abba6a3b5a67b02249c9
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-17 23:14:07 +0530

    2nd largest element

---
+++ Java_CodingInterview/src/practice/java/TestJava.java
@@ -0,0 +1,35 @@
+package practice.java;
+
+public class TestJava
+{
+	public static void main()
+	{
+	    int a[] = {7,5,6,8,9,3};
+	 
+	    int max = a[0];
+	    int secMax = 0;
+	    
+	    for(int i=1;i<a.length;i++)
+	    {
+	      if(max<a[i])
+	      {
+	        secMax=max;
+	        max=a[i];
+	      }
+	      else 
+	      {
+	         if(secMax==0)
+	         {
+	           secMax=a[i];
+	         }
+	         else if(secMax<a[i])
+	         {
+	           secMax=a[i];
+	         }
+	      }
+	    }
+	    System.out.println(secMax);
+	    
+	}  
+		
+}
