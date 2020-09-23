9c8fbb0d71acb047c15be46877b68c731af4a29a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-23 23:31:58 +0530

    Frequency of elements in array

---
+++ Java_CodingInterview/src/problems/FrequencyArrayBitwise.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class FrequencyArrayBitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = {3,4,3,7,3,4,5,7,4,5,7};
+		
+		int allXOR;
+		
+		System.out.println(3^3^3);
+		
+		System.out.println(3^7);
+		
+	}
+
+}
