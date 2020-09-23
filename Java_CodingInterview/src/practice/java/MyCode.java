b5f53f4c7eafe71f0d1e585c1e13e5daba7e180f
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-23 22:03:28 +0530

    Frequency of elements in array

--- Java_CodingInterview/src/practice/java/MyCode.java
+++ Java_CodingInterview/src/practice/java/MyCode.java
@@ -1,26 +1,37 @@
 package practice.java;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class MyCode 
 {
 	public static void main (String[] args) 
 	{
 		System.out.println("Hello Java");
-    int input1[]={1,1,1,5,5,5,6,6,6,7,7};
+        int input1[]={1,1,1,5,5,5,6,6,6,7,7};
     
     Map<Integer,Integer> result = new HashMap<Integer,Integer>();
     
-    for(int i=0;i<input1.length;i++){
+    for(int i=0;i<input1.length;i++)
+    {
       
       if(result.containsKey(input1[i])){
         result.put(input1[i],result.get(input1[i])+1);
       }
-      else{
+      else
+      {
         result.put(input1[i],1);
       }
       
-    }
+    }   
     
-    
-    List<Integer> r= result.stream().filter(s->s.get()<3).Collect(s.key());	
+//        List<Integer> r  =  result.stream().filter(s->s.get() < 3).collect(s.key());		
+   }
 	
-}
+//	SDK Platform Android 10, API 29, revision 4
+	
+//	List<Integer> r  =  result.stream().filter(s->s.get() < 3).collect(s.key());
+
+	
+}	
--- Java_CodingInterview/src/practice/java/TestJava.java
+++ Java_CodingInterview/src/practice/java/TestJava.java
@@ -30,28 +30,9 @@ public class TestJava
 	    }
 	    System.out.println(secMax);
 	    
-	}  
+	}
+ }	
 	
-	class MyCode 
-	{
-		public static void main (String[] args) 
-		{
-			System.out.println("Hello Java");
-	    int input1[]={1,1,1,5,5,5,6,6,6,7,7};
-	    
-	    Map<Integer,Integer> result = new HashMap<Integer,Integer>();
-	    for(int i=0;i<input1.length;i++){
-	      
-	      if(result.containsKey(input1[i])){
-	        result.put(input1[i],result.get(input1[i])+1);
-	      }else{
-	        result.put(input1[i],1);
-	      }
-	      
-	    }
-	    
-	    
-	    List<Integer> r= result.stream().filter(s->s.get()<3).Collect(s.key());	
-		
-}
+	
+	
 		
