8078982e866969502d72def28ddc84b0e9a67724
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-23 22:17:04 +0530

    Frequency of elements in array

--- Java_CodingInterview/src/problems/FrequencyElement.java
+++ Java_CodingInterview/src/problems/FrequencyElement.java
@@ -1,10 +1,33 @@
 package problems;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class FrequencyElement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        		
+		int arr[] = {3,4,3,7,3,4,5,7,4,5,7};
+		
+//		int len = arr.length;
+		
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		
+		for(int i = 0;i < arr.length;i++)
+		{
+			if(map.containsKey(arr[i]))
+				map.put(arr[i],map.get(arr[i])+1);
+			else
+				map.put(arr[i],1);
+		}
+		
+		Set<Entry<Integer, Integer>> entry = map.entrySet();
+		
+		
+		
 	}
 
 }
