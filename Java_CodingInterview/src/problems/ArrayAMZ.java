bcaeb9268a1a1c2945a4c7fc32a30ecf6168aa55
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-24 13:07:23 +0530

    @Amazon array problem

---
+++ Java_CodingInterview/src/problems/ArrayAMZ.java
@@ -0,0 +1,25 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayAMZ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = {1,2,3,4,5,5,5,4,3,10};
+		
+		Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+		
+		
+		for(int i = 0;i < arr.length;i++)
+		{
+			
+		}
+		
+		
+		
+	}
+
+}
