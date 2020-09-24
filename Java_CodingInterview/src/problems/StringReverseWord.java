24b6a23efa0610dd2598a8b7a8b5c63a18684b87
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-24 19:10:55 +0530

    @String problem

--- Java_CodingInterview/src/problems/StringReverseWord.java
+++ Java_CodingInterview/src/problems/StringReverseWord.java
@@ -5,6 +5,15 @@ public class StringReverseWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String str = "I am going home";
+		
+		String str1[] = str.split("/s");
+		
+		int len = str1.length;
+		
+		while(len >= 0)
+			System.out.println(str1[len--]);
+		
 	}
 
 }
