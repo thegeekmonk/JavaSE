afc353b00232dfa0cfe8cbcda5aef9ab41d91eca
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-25 13:09:39 +0530

    @String word reversal problem

--- Java_CodingInterview/src/problems/StringReversalWord.java
+++ Java_CodingInterview/src/problems/StringReversalWord.java
@@ -8,28 +8,40 @@ public class StringReversalWord {
 		// TODO Auto-generated method stub
 
 		String str = "How  are   you";
+		StringBuilder sb = new StringBuilder();
 		
 		int len = str.length();
 		
-		int start = 0,end = 0;		
-		int i = 0;
+		int start = 0,end = 0;	
+		char pre,next;
+		int i;
+		
 		
 		Stack<String> stack = new Stack<String>();
 		
+		i = 0;
+		pre =  str.charAt(0);
+		next = str.charAt(0);
+		
 		while(i < len)
 		{
-			if(str.charAt(i) == ' ')
+			if(pre != ' ' && str.charAt(i) == ' ')
 			{
 				end = i;
 				stack.push(str.substring(start, end));
-				
-				start = i;
 			}
+			else if(pre == ' ' && str.charAt(i) != ' ')
+				start = i;
 			
+			pre = str.charAt(i); 
 			
 			i++;
 		}
 		
+		while(!stack.empty())
+			sb.append(stack.pop());
+		
+		System.out.println("Reversed String : "+sb);
 	}
 
 }
