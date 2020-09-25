f24db089971c28c658bb879a87a7bd587dce5da8
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-09-25 12:23:58 +0530

    @String problem

---
+++ Java_CodingInterview/src/problems/StringReversalWord.java
@@ -0,0 +1,14 @@
+package problems;
+
+public class StringReversalWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "How  are   you";
+		
+		int len; 
+		
+	}
+
+}
