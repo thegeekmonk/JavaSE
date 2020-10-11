23b6716e20709c43dde3621d87b0b5a634c231e5
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-11 15:48:26 +0530

    Regular Expression 1

--- 101_Java_Regex_1/src/regularexp/Regex1.java
+++ 101_Java_Regex_1/src/regularexp/Regex1.java
@@ -1,5 +1,7 @@
 package regularexp;
 
+import java.util.regex.Pattern;
+
 public class Regex1 {
 
 	public Regex1() {
@@ -8,7 +10,9 @@ public class Regex1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        
+		Pattern p = Pattern.compile(".s");
+		
 	}
 
 }
