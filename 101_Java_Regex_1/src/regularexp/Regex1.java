987d8865b6494ae437fde99f0ff1050ccc3b45fc
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-11 15:45:44 +0530

    Regular Expression 1

---
+++ 101_Java_Regex_1/src/regularexp/Regex1.java
@@ -0,0 +1,14 @@
+package regularexp;
+
+public class Regex1 {
+
+	public Regex1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
