295c94acb0d1b526313c0093308e8af31450c00d
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-11 16:30:04 +0530

    Regular Expression 1

--- 101_Java_Regex_1/src/regularexp/RegexCharacterClass.java
+++ 101_Java_Regex_1/src/regularexp/RegexCharacterClass.java
@@ -1,10 +1,15 @@
 package regularexp;
 
+import java.util.regex.Pattern;
+
 public class RegexCharacterClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(Pattern.matches("[abcd]","dcba"));
+		
+		
 	}
 
 }
