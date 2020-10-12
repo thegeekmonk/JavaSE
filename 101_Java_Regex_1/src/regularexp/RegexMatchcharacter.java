d9fabf5b9a9ff301566f934cfeb2ff380bc1638e
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-12 20:39:56 +0530

    Regular Expression 3

--- 101_Java_Regex_1/src/regularexp/RegexMatchcharacter.java
+++ 101_Java_Regex_1/src/regularexp/RegexMatchcharacter.java
@@ -1,10 +1,16 @@
 package regularexp;
 
+import java.util.regex.Pattern;
+
 public class RegexMatchcharacter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(Pattern.matches("\\d","abc"));
+		
+		
+		
 	}
 
 }
