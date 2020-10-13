d8daf4eaa059092664d78014f90bf3129a83f64e
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-13 15:51:26 +0530

    Regular Expression 3

--- 101_Java_Regex_1/src/regularexp/RegexExample1.java
+++ 101_Java_Regex_1/src/regularexp/RegexExample1.java
@@ -1,10 +1,13 @@
 package regularexp;
 
+import java.util.regex.Pattern;
+
 public class RegexExample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      
+		System.out.println(Pattern.matches("[789][0-9]{5}","712348"));
 	}
 
 }
