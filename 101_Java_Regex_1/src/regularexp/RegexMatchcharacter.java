1aa09aea4f1be2884b794742dbdccb18428e2f05
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-13 15:45:43 +0530

    Regular Expression 3

--- 101_Java_Regex_1/src/regularexp/RegexMatchcharacter.java
+++ 101_Java_Regex_1/src/regularexp/RegexMatchcharacter.java
@@ -7,17 +7,17 @@ public class RegexMatchcharacter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(Pattern.matches("\\d","abc"));
-		System.out.println(Pattern.matches("\\d","12"));
-		System.out.println(Pattern.matches("\\d","5"));
-		
-		
-		System.out.println(Pattern.matches("\\D","abc"));
-		System.out.println(Pattern.matches("\\D","a"));
-		System.out.println(Pattern.matches("\\D","1"));
-		
-		System.out.println(Pattern.matches("\\D+", "abcdrisbdjgugu"));
-		System.out.println(Pattern.matches("\\D*",""));
+//		System.out.println(Pattern.matches("\\d","abc"));
+//		System.out.println(Pattern.matches("\\d","12"));
+//		System.out.println(Pattern.matches("\\d","5"));
+//		
+//		
+//		System.out.println(Pattern.matches("\\D","abc"));
+//		System.out.println(Pattern.matches("\\D","a"));
+//		System.out.println(Pattern.matches("\\D","1"));
+//		
+//		System.out.println(Pattern.matches("\\D+", "abcdrisbdjgugu"));
+//		System.out.println(Pattern.matches("\\D*",""));
 		
 	}
 
