089b8402c82a10b08fa3715d9e319d13eb748847
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-13 15:46:22 +0530

    Regular Expression 3

--- 101_Java_Regex_1/src/regularexp/RegexMatchcharacter.java
+++ 101_Java_Regex_1/src/regularexp/RegexMatchcharacter.java
@@ -19,6 +19,8 @@ public class RegexMatchcharacter {
 //		System.out.println(Pattern.matches("\\D+", "abcdrisbdjgugu"));
 //		System.out.println(Pattern.matches("\\D*",""));
 		
+		System.out.println(Pattern.matches("\\D*","kssj"));
+		
 	}
 
 }
