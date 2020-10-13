e76c5b775d66a4616f074154941d08422883a62a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-13 15:54:43 +0530

    Regular Expression 3

--- 101_Java_Regex_1/src/regularexp/RegexExample1.java
+++ 101_Java_Regex_1/src/regularexp/RegexExample1.java
@@ -8,6 +8,7 @@ public class RegexExample1 {
 		// TODO Auto-generated method stub
       
 		System.out.println(Pattern.matches("[789][0-9]{5}","712348"));
+		System.out.println(Pattern.matches("[789][0-9]{9}","7123487877"));
 	}
 
 }
