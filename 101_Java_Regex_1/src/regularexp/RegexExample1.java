048a8bc86acfd3a21956cdaefc4977c4f3ff01b5
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-13 16:21:56 +0530

    Regular Expression 3

--- 101_Java_Regex_1/src/regularexp/RegexExample1.java
+++ 101_Java_Regex_1/src/regularexp/RegexExample1.java
@@ -7,8 +7,15 @@ public class RegexExample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       
-		System.out.println(Pattern.matches("[789][0-9]{5}","712348")); //string should start with either 7,8,9 and total length 5+1 : 6
-		System.out.println(Pattern.matches("[789][0-9]{9}","7123487877"));
+		
+		//Regex Quantifiers
+		System.out.println(Pattern.matches("[789][0-9]{5}","712348")); //start with either 7,8,9 and total length 5+1 : 6
+		System.out.println(Pattern.matches("[789][0-9]{9}","7123487877"));//start with either 7,8,9 and total length 9+1 : 10
+		
+	    //Regex Metacharacters
+		
+		System.out.println(Pattern.matches("[6789]{1}\\d{9}","6809399734"));
+		System.out.println(Pattern.matches("[89]{1}\\d{9}","6809399734"));
 	}
 
 }
