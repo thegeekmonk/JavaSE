af722259a82df80c8a385181152683e0d369cf4f
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-10-13 16:50:46 +0530

    Regular Expression Finder

--- 101_Java_Regex_1/src/regularexp/RegexFinder.java
+++ 101_Java_Regex_1/src/regularexp/RegexFinder.java
@@ -1,10 +1,25 @@
 package regularexp;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexFinder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Regular Expression Example :-");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true)
+		{
+			Pattern pattern = Pattern.compile(sc.nextLine());
+			
+			Matcher matcher = pattern.matcher(sc.nextLine());
+		}
+		
 	}
 
 }
