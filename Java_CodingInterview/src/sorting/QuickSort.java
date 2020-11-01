477eb7a90db2ff4db37e18a3abc790152f5ef38d
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-11-01 19:32:40 +0530

    @QuickSort practice

--- Java_CodingInterview/src/sorting/QuickSort.java
+++ Java_CodingInterview/src/sorting/QuickSort.java
@@ -2,7 +2,18 @@ package sorting;
 
 public class QuickSort 
 {
-	public static void main(String[] args) {
+	
+	public static void partition()
+	{
+		
+	}
+	public static void Quicksort()
+	{
+		
+	}	
+	
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
