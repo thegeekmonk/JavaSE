516f712fc37d243d5f44aa508b88d0eaa01ae3c8
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-07-19 17:05:12 +0530

    lexicographical order string @hackerrank

--- Java_CodingInterview/src/hackerrank/LexicographicOrderString.java
+++ Java_CodingInterview/src/hackerrank/LexicographicOrderString.java
@@ -1,15 +1,25 @@
 package hackerrank;
 
+import java.util.TreeSet;
+
 public class LexicographicOrderString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 		String str = "welcometojava";
 		
 		int len = str.length();
+		int i = 0;
 		
-
+		TreeSet<String> sortedSet = new TreeSet<String>();
+		
+		while(i <= len-3)
+		{
+			sortedSet.add(str.substring(i, i+3));			
+		}
+        		
+        System.out.println("First Lexiorder String : "+sortedSet.first());
+        System.out.println("Last Lexiorder String : "+sortedSet.last());
 	}
 
 }
