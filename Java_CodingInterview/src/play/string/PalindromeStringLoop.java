3065d13f6a5678f0b967e6fd709032b9dbc90847
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-07-19 13:43:33 +0530

    string palindrome check using loop

---
+++ Java_CodingInterview/src/play/string/PalindromeStringLoop.java
@@ -0,0 +1,31 @@
+package play.string;
+
+public class PalindromeStringLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "apmpa";
+		
+		int i = 0;
+		int j = str.length();
+		
+		
+		while(i< j)
+		{
+			if(str.charAt(i) == str.charAt(j))
+			{
+				i++;
+				j++;
+			}
+			else
+			{
+				System.out.println("Not a Palindrome String");
+			}
+		}
+		if(i > j || i == j)
+		{
+			System.out.println("String is Palindrome");
+		}
+	}
+}
