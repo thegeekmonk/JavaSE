ed37dc294dc66577abd1751b9e4452685a7eadb1
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-07-19 14:13:44 +0530

    string palindrome check using @recursion

---
+++ Java_CodingInterview/src/play/string/PalindromeStringRecursion.java
@@ -0,0 +1,22 @@
+package play.string;
+
+import java.util.Scanner;
+
+public class PalindromeStringRecursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		System.out.println("Is String "+str+" Palindrome : "+checkPalindrome(str));
+	}
+	
+	static boolean checkPalindrome(String word)
+	{
+	
+		
+		return false;
+	}
+
+}
