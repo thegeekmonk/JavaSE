852429ff4d3b9ce79d2cd560e19039b161d2c753
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-07-19 17:00:52 +0530

    lexicographical order string @hackerrank

---
+++ Java_CodingInterview/src/hackerrank/LexicographicOrderString.java
@@ -0,0 +1,15 @@
+package hackerrank;
+
+public class LexicographicOrderString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "welcometojava";
+		
+		int len = str.length();
+		
+
+	}
+
+}
