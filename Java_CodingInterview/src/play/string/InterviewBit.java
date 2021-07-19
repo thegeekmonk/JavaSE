f46019087b30799065af646f9cd59bf27f598b42
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-07-19 15:10:58 +0530

    string palindrome check using @recursion

---
+++ Java_CodingInterview/src/play/string/InterviewBit.java
@@ -0,0 +1,14 @@
+package play.string;
+
+class InterviewBit {
+    public void method1 (int num1,float num2){
+         System.out.println("int-float method");
+    }
+//    public void method1(float num1,int num2){
+//         System.out.println("float-int method");
+//    }
+     public static void main(String[] args){
+           InterviewBit interviewBit=new InterviewBit();
+           interviewBit.method1(40,20);
+     }
+}
