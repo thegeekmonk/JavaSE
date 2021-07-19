cacc704026a68a6a41a62750d784cd6b57f0fd1a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-07-19 13:58:45 +0530

    string palindrome check using loop

--- Java_CodingInterview/src/play/string/PalindromeStringLoop.java
+++ Java_CodingInterview/src/play/string/PalindromeStringLoop.java
@@ -4,7 +4,7 @@ public class PalindromeStringLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
-		String str = "appa";		
+		String str = "appam";		
 		int i = 0;
 		int j = str.length() - 1;
 				
@@ -18,6 +18,7 @@ public class PalindromeStringLoop {
 			else
 			{
 				System.out.println("Not a Palindrome String");
+				break;
 			}
 		}
 		
