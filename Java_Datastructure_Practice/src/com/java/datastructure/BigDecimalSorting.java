855529736bc3ce3a1a46c949bca653425d3b0cff
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-11 22:34:18 +0530

    BigDecimal data sorting

--- Java_Datastructure_Practice/src/com/java/datastructure/BigDecimalSorting.java
+++ Java_Datastructure_Practice/src/com/java/datastructure/BigDecimalSorting.java
@@ -1,10 +1,25 @@
 package com.java.datastructure;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BigDecimalSorting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub     
+	ArrayList<BigDecimal> list = new ArrayList<BigDecimal>();	
+	
+	String str[] = {".12","1.12","0.11"};
+		
+	for(String st : str)
+	{
+		list.add(new BigDecimal(st));
+	}
+	
+	Collections.sort(list);
+	
+	System.out.println(list);
 	}
 
 }
