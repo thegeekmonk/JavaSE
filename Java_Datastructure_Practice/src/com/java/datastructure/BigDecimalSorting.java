56eb2a72f9050a68f202d710108b9b38a92259fe
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-11 22:55:57 +0530

    BigDecimal data sorting

--- Java_Datastructure_Practice/src/com/java/datastructure/BigDecimalSorting.java
+++ Java_Datastructure_Practice/src/com/java/datastructure/BigDecimalSorting.java
@@ -3,6 +3,36 @@ package com.java.datastructure;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+
+class DecimalComparator implements Comparator<BigDecimal>
+{   
+	BigDecimal bg;
+	
+	DecimalComparator()
+	{
+		//this.bg = bg;
+	}
+//	public int compare(BigDecimal bg1,BigDecimal bg2)
+//	{
+//		if(bg1 == bg2)
+//		{
+//			
+//		}
+//		
+//		return bg1.compareTo(bg2);
+//	}
+	@Override
+	public int compare(BigDecimal bg1, BigDecimal bg2) {
+		// TODO Auto-generated method stub		
+		
+		if(bg1 == bg2)
+		{
+			return 0;
+		}		
+		return bg1.compareTo(bg2);
+	}
+}
 
 public class BigDecimalSorting {
 
@@ -17,7 +47,7 @@ public class BigDecimalSorting {
 		list.add(new BigDecimal(st));
 	}
 	
-	Collections.sort(list);
+	Collections.sort(list,new DecimalComparator());
 	
 	System.out.println(list);
 	}
