aa767e1e40c7a485cf25fa69d4ab6645d40d8503
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-18 17:36:25 +0530

    @Inner class practice

--- JavaPractice/src/com/java/se/Outer.java
+++ JavaPractice/src/com/java/se/Outer.java
@@ -4,6 +4,13 @@ public class Outer {
 	
 	private int data;
 	
+	class Inner
+	{
+		public void msg()
+		{
+			System.out.println("Outer class Private data : "+data);
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
