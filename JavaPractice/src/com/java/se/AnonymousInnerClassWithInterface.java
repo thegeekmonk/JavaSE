d88dce45001bb8f266c3b342f1e6dd4593a32d5a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-19 02:20:29 +0530

    @StaticNested class

--- JavaPractice/src/com/java/se/AnonymousInnerClassWithInterface.java
+++ JavaPractice/src/com/java/se/AnonymousInnerClassWithInterface.java
@@ -16,5 +16,4 @@ class AnonymousInnerClassWithInterface {
        };
        p.eat();
 	}
-
 }
---
+++ JavaPractice/src/com/java/se/StaticNestedClass.java
@@ -0,0 +1,19 @@
+package com.java.se;
+
+public class StaticNestedClass {
+   
+	static int data = 90;
+	static class InnerClass
+	{
+		void msg()
+		{
+			System.out.println("Data Is : "+data);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
