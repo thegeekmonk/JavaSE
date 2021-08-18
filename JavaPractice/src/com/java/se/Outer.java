a25d5ef61cb697c688213c09ef97d2c4238278fc
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-18 17:32:10 +0530

    @Inner class practice

---
+++ JavaPractice/src/com/java/se/Outer.java
@@ -0,0 +1,13 @@
+package com.java.se;
+
+public class Outer {
+	
+	private int data;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java_Datastructure_Practice/src/com/java/datastructure/BigDecimalSorting.java
+++ Java_Datastructure_Practice/src/com/java/datastructure/BigDecimalSorting.java
@@ -13,15 +13,16 @@ class DecimalComparator implements Comparator<BigDecimal>
 	{
 		//this.bg = bg;
 	}
+	
 //	public int compare(BigDecimal bg1,BigDecimal bg2)
 //	{
 //		if(bg1 == bg2)
 //		{
-//			
-//		}
-//		
+//		     	
+//		}	
 //		return bg1.compareTo(bg2);
 //	}
+	
 	@Override
 	public int compare(BigDecimal bg1, BigDecimal bg2) {
 		// TODO Auto-generated method stub		
