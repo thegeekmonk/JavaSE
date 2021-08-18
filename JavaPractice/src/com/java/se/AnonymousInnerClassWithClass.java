11b5902e54fbd724b6286f382b0e1337e6004225
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-19 01:17:02 +0530

    @AnonymousInner class with class

---
+++ JavaPractice/src/com/java/se/AnonymousInnerClassWithClass.java
@@ -0,0 +1,15 @@
+package com.java.se;
+
+abstract class Person
+{
+	abstract void eat();
+}
+
+public class AnonymousInnerClassWithClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
