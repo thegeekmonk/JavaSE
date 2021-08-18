0891d06977a97a209398f06211ea7d81e6962751
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-18 17:40:47 +0530

    @Inner class practice

--- JavaPractice/src/com/java/se/Outer.java
+++ JavaPractice/src/com/java/se/Outer.java
@@ -14,7 +14,11 @@ public class Outer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      Outer out = new Outer();
+	  
+      Outer.Inner in = out.new Inner();
+      
+      in.msg();
 	}
 
 }
