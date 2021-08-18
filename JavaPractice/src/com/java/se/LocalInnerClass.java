1938f1c8f1a170ff1f5d4a975c7e7399b6842eac
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-19 00:54:20 +0530

    @LocalInner class practice

--- JavaPractice/src/com/java/se/LocalInnerClass.java
+++ JavaPractice/src/com/java/se/LocalInnerClass.java
@@ -1,7 +1,23 @@
 package com.java.se;
 
 public class LocalInnerClass {
-
+    
+	private int data = 90;
+	
+	void display()
+	{
+		class LocalInner
+		{
+			void msg()
+			{
+				System.out.println("Inner Class Data : "+data);
+			}
+		}
+		
+		LocalInner in = new LocalInner();
+		in.msg();
+	}
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
