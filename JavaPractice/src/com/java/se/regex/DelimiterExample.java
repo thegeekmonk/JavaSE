a67bf24ae97b517ed18f9e89958fbe7f4dd3cefc
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-21 20:39:53 +0530

    @java regex practice

--- JavaPractice/src/com/java/se/regex/DelimiterExample.java
+++ JavaPractice/src/com/java/se/regex/DelimiterExample.java
@@ -7,7 +7,9 @@ public class DelimiterExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       
-		String str1 = "Java is a !good programming language";
+		//String str1 = "Java is a !good programming language";
+		
+		String str1 = "If you don't like something, change.it.";   
 		
 //		Scanner sc = new Scanner(str1);
 //		
@@ -16,7 +18,7 @@ public class DelimiterExample {
 //			System.out.println(sc.next());
 //		}
 		
-		String arr[] = str1.split("!");
+		String arr[] = str1.split(",");
 		
 		for(String str : arr)
 		{
