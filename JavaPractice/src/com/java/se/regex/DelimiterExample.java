836b0b03b7f08b04ed08d4f3ce83430bba983c49
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-21 20:25:27 +0530

    @java regex practice

--- JavaPractice/src/com/java/se/regex/DelimiterExample.java
+++ JavaPractice/src/com/java/se/regex/DelimiterExample.java
@@ -1,10 +1,17 @@
 package com.java.se.regex;
 
+import java.util.Scanner;
+
 public class DelimiterExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       
+		String str1 = "Java is a good programming language";
+		
+		Scanner sc = new Scanner(str);
+		
+		
 	}
 
 }
