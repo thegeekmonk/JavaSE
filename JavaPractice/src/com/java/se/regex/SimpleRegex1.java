a5de2d767141e950cc167a3cc8a7a869fee3e07a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-22 19:18:27 +0530

    @java regex

--- JavaPractice/src/com/java/se/regex/SimpleRegex1.java
+++ JavaPractice/src/com/java/se/regex/SimpleRegex1.java
@@ -8,11 +8,15 @@ public class SimpleRegex1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      
-		Pattern p = Pattern.compile("..\\d");
-		Matcher m = p.matcher("788");
+//		Pattern p = Pattern.compile("..\\d");
+//		Matcher m = p.matcher("788");
+//		
+//		boolean b = m.matches();		
+//		System.out.println("Result : "+b);
 		
-		boolean b = m.matches();		
+		boolean b = Pattern.matches(".....G","HelloG");
+		
 		System.out.println("Result : "+b);
+		
 	}
-
 }
