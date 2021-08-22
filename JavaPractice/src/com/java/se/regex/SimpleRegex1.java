509ee7a1e98564d6242548f583e350fc4bacd718
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-22 18:53:34 +0530

    @java regex

--- JavaPractice/src/com/java/se/regex/SimpleRegex1.java
+++ JavaPractice/src/com/java/se/regex/SimpleRegex1.java
@@ -1,10 +1,18 @@
 package com.java.se.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleRegex1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     
+		Pattern p = Pattern.compile("..d");
+		Matcher m = p.matcher("78d");
+		
+		boolean b = m.matches();		
+		System.out.println("Result : "+b);
 	}
 
 }
