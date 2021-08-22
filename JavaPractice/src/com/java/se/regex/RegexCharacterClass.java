1179d3aa3f48bce87b8cadec0c575bf37eec4909
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-22 19:55:45 +0530

    @java regex

--- JavaPractice/src/com/java/se/regex/RegexCharacterClass.java
+++ JavaPractice/src/com/java/se/regex/RegexCharacterClass.java
@@ -7,7 +7,7 @@ public class RegexCharacterClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        
-		boolean b = Pattern.matches("[amn]","a");
+		boolean b = Pattern.matches("[amn]","am");
 		
 		System.out.println("Result : "+b);
 		
