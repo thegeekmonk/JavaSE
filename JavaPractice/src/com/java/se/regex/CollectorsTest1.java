fc23a399037d2d3131db9db40f2accf3cb08c3c8
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-28 14:08:45 +0530

    @java 1.8 @Collectors

--- JavaPractice/src/com/java/se/regex/CollectorsTest1.java
+++ JavaPractice/src/com/java/se/regex/CollectorsTest1.java
@@ -1,10 +1,21 @@
 package com.java.se.regex;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Product
 {
 	int id;
 	String name;
 	float price;
+	
+	Product(int id, String name,float price)
+	{
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -24,16 +35,25 @@ class Product
 		this.price = price;
 	}
 	
-	
 }
-
-
 
 public class CollectorsTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Product p1 = new Product(101,"HP",2200);
+		Product p2 = new Product(101,"Dell",2100);
+		Product p3 = new Product(101,"Lenevo",2300);
+		Product p4 = new Product(101,"Apple",2400);
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+		
 	}
 
 }
