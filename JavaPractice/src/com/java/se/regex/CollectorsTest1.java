d9b57b09cbcdf871c56e06ee8eac04ac5e8a4d25
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-28 14:02:03 +0530

    @java 1.8 @Collectors

--- JavaPractice/src/com/java/se/regex/CollectorsTest1.java
+++ JavaPractice/src/com/java/se/regex/CollectorsTest1.java
@@ -1,5 +1,34 @@
 package com.java.se.regex;
 
+class Product
+{
+	int id;
+	String name;
+	float price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
+
+
+
 public class CollectorsTest1 {
 
 	public static void main(String[] args) {
