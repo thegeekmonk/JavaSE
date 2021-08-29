de2c033a09696e4bc88087d7492c43de92612e15
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-08-29 13:53:16 +0530

    @java 1.8 lambda

--- JavaPractice/src/com/java/se/Java8Stream1.java
+++ JavaPractice/src/com/java/se/Java8Stream1.java
@@ -1,10 +1,27 @@
 package com.java.se;
 
+interface Sayable
+{
+	public String say();
+}
+
+
 public class Java8Stream1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Sayable s1 = new Sayable() {             
+			public String say()
+			{
+				return "Without Lambda Expression";
+			}
+		};
+		
+		System.out.println(s1.say());
+		
+		
+		
 	}
 
 }
