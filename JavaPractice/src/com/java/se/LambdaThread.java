62d12bd5dee94d82646b225837cb72b70fafd9e3
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-01 22:35:53 +0530

    @java 1.8 lambda

--- JavaPractice/src/com/java/se/LambdaThread.java
+++ JavaPractice/src/com/java/se/LambdaThread.java
@@ -4,7 +4,20 @@ public class LambdaThread {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        
+		Runnable r1 = new Runnable()
+				{
+			         public void run()
+			         {
+			        	 System.out.println("Thread without lambda is running");
+			         }
+				};
+				
+	    Thread t1 = new Thread(r1);
+	    t1.start();
+		
+		
+		
+		
 	}
-
 }
