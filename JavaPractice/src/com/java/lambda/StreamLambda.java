7b56919ecd5f1f88fb929aeb6f8e155d21579a89
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-03 16:17:21 +0530

    @java 1.8 lambda

--- JavaPractice/src/com/java/lambda/StreamLambda.java
+++ JavaPractice/src/com/java/lambda/StreamLambda.java
@@ -1,5 +1,8 @@
 package com.java.lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Prod
 {
 	int id;
@@ -37,7 +40,22 @@ public class StreamLambda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		List<Prod> list = new ArrayList<>();
+		
+		Prod p1 = new Prod(1,"IPhone XR",52000f);
+		Prod p2 = new Prod(2,"IPhone 8",47000f);
+		Prod p3 = new Prod(3,"Moto G2",14000f);
+		Prod p4 = new Prod(4,"Galaxy POP",8500f);
+		
+		list.add(p1);
+		list.add(p2);
+		list.add(p3);
+		list.add(p4);
+		
+		Prod pr = list.stream().max((product1,product2)->p1.price > p2.price ?1:-1).get();
+		
+		System.out.println(p2.name);
+		
 	}
 
 }
