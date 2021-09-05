caac66930f34b13e74298a904eb70b54b7bb0386
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-05 18:05:22 +0530

    @java8 stream practice

--- Java 8 Practice/src/practice/java8/StreamLambda.java
+++ Java 8 Practice/src/practice/java8/StreamLambda.java
@@ -1,5 +1,41 @@
 package practice.java8;
 
+class Product
+{
+	int id;
+	String name;
+	float price;
+	
+	Product(int id, String name,float price)
+	{
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+}
+
+
+
 public class StreamLambda {
 
 	public static void main(String[] args) {
