1326cf1c0b62adf8dc4533f7820fa6a2fdc27d24
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-05 19:19:18 +0530

    @java8 stream practice

--- Java 8 Practice/src/practice/java8/StreamLambda.java
+++ Java 8 Practice/src/practice/java8/StreamLambda.java
@@ -68,6 +68,12 @@ public class StreamLambda {
 		
 		Set<Float> set = list.stream().filter(k -> k.price >30000f).map(Product::getPrice).collect(Collectors.toSet());
 		System.out.println(set);
+		
+		
+		Product pmax = list.stream().max((p1,p2)-> p1.price > p2.price ? 1: -1).get();
+		System.out.println("Maximum "+pmax.price);
+		
+		
 	}
 
 }
