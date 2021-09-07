6b5b88c826290480298a6a55ba49c3e1b3d05cef
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-07 08:50:08 +0530

    @java8 stream practice

--- Java 8 Practice/src/practice/java8/StreamFirstNonRepeatCharInString.java
+++ Java 8 Practice/src/practice/java8/StreamFirstNonRepeatCharInString.java
@@ -1,10 +1,26 @@
 package practice.java8;
 
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class StreamFirstNonRepeatCharInString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String str = "aaplee";
+		
+		Character result = str.chars().mapToObj(s->Character.toLowerCase(Character.valueOf((char)s)))
+				                      .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
+				                      .entrySet()
+				                      .stream()
+				                      .filter(i->i.getValue() == 1L)
+				                      .map(j->j.getKey())
+				                      .findFirst()
+				                      .get();
+		System.out.println("1st Non Repeatative Char : "+result);
+		
 	}
 
 }
