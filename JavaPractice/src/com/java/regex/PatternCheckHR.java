de596e97ea62d52fb6e2a2c5ddec943cd5ec9251
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-09 23:55:16 +0530

    @regex pattern check @HR

--- JavaPractice/src/com/java/regex/PatternCheckHR.java
+++ JavaPractice/src/com/java/regex/PatternCheckHR.java
@@ -5,6 +5,11 @@ public class PatternCheckHR {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String str = "Hello";
+		
+		System.out.println(str.hashCode());
+		
+		
 	}
 
 }
