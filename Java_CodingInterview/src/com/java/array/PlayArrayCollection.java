9525266193a90809f5a73d7ae2381ba51707b4a4
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-13 23:10:46 +0530

    @Array abd Collection API

---
+++ Java_CodingInterview/src/com/java/array/PlayArrayCollection.java
@@ -0,0 +1,22 @@
+package com.java.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayArrayCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> list = new ArrayList<Integer>();
+		
+		list.add(89);
+		list.add(11);
+		list.add(34);
+		list.add(23);
+		
+		System.out.println(list);
+		
+	}
+
+}
