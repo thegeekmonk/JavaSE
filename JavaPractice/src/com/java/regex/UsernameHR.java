78ab9464bb56a5870c0f963dde46eba63732c5da
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-09-13 23:53:19 +0530

    @HR regex problem

--- JavaPractice/src/com/java/regex/UsernameHR.java
+++ JavaPractice/src/com/java/regex/UsernameHR.java
@@ -1,10 +1,16 @@
 package com.java.regex;
 
+import java.util.regex.Pattern;
+
 public class UsernameHR {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(Pattern.matches("[a-zA-z]\\w{7,29}","julia"));
+		
+		
+		
 	}
 
 }
